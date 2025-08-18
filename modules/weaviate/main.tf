0620dfdab0fd6696de0427118f6241b9837f91f0
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-19 01:08:25 +0300

    update

--- modules/weaviate/main.tf
+++ modules/weaviate/main.tf
@@ -1,53 +1,22 @@
-
-resource "helm_release" "weaviate" {
-  name             = "weaviate"
-  namespace        = "weaviate"
+resource "helm_release" "distributed_chroma" {
+  name       = "distributed-chroma"
+  namespace  = "chroma"
+  repository = "https://charts.chroma.com"  # replace with actual Helm repo URL
+  chart      = "distributed-chroma"
+  version    = "0.1.0"                       # replace with desired chart version
   create_namespace = true
-  repository       = "https://weaviate.github.io/weaviate-helm"
-  chart            = "weaviate"
-  version          = "17.5.1"  # latest available chart version
-  timeout          = 600
 
-  # Service types
+  #values = [
+  #  file("${path.module}/values.yaml")       # optional, your custom config
+  #]
+
+  set {
+    name  = "replicaCount"
+    value = 3
+  }
+
   set {
     name  = "service.type"
     value = "ClusterIP"
-  }
-
-  set {
-    name  = "grpcService.type"
-    value = "ClusterIP"
-  }
-
-  # Authentication
-  #set {
-  #  name  = "authentication.apikey.enabled"
-  #  value = "true"
-  #}
-
-  #set {
-  #  name  = "authentication.apikey.allowed_keys[0]"
-  #  value = "\"z3EkNNu8HnEotF2JK2xNlG38xXOtRR6Ns3jEQsM5J3c=\""
-  #}
-
-  #set {
-  #  name  = "authentication.apikey.users[0].username"
-  #  value = "admin"
-  #}
-
-  #set {
-  #  name  = "authentication.apikey.users[0].key"
-  #  value = "\"z3EkNNu8HnEotF2JK2xNlG38xXOtRR6Ns3jEQsM5J3c=\""
-  #}
-
-  set {
-    name  = "authentication.anonymous_access.enabled"
-    value = "true"
-  }
-
-  # Log level
-  set {
-    name  = "logLevel"
-    value = "info"
   }
 }
